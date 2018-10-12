6b15d00a5b211a0d5232c278ef213045e747cb24
Miles Cole <milescole@Miless-MacBook-Pro.local>
2018-10-12 11:32:10 -0700

    Add backend skeleton

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,25 @@
+// Main Entry Point File
+
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+
+const users = require('./routes/users');
 
 const app = express();
 
-const PORT = 3000;
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Use route for /users
+app.use('/users', users);
+
+// Serve bundled index.html at root
 app.use(express.static(path.resolve(__dirname, '../../dist/')));
+
+// When deployed use the first part || localhost:3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (err) => {
   if (err) console.error('Server Error - ', err);
