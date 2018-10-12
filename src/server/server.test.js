e200278a9ea5282c775962e3fcca039ed4e2ff54
Serge Vartanov <vartanov.s@gmail.com>
2018-10-12 15:25:57 -0700

    Progress on Server Test - Stuck w/Babel

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -1,2 +1,11 @@
 const request = require('supertest');
 const app = require('server.js');
+
+describe('Testing Root Path', () => {
+  test('It should respond to GET method', (done) => {
+    request(app).get('/').then((res) => {
+      expect(res.statusCode).toBe(200);
+      done();
+    });
+  });
+});
