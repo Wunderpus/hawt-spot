4b49bbab421429473241984290817f4aefd4365b
Emeka Okonkwo <emekaokonkwo@Emekas-MacBook-Pro.local>
2018-10-12 15:32:27 -0700

    register-login-start

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import MainContainer from './components/MainContainer';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      username: 'name',
+      password: 'password',
+    };
   }
 
   render() {
     return (
       <div>
         <h1>Welcome to Hawt-Spot!!!</h1>
+        <MainContainer username={this.state.username} password={this.state.password} />
       </div>
     );
   }
